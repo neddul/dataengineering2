22edafb155d88f232195293ee2183276f385c559
neddul <ludvigwesterholm98@gmail.com>
2024-04-25 17:17:05 +0000

    now works to start pulsar from any directory

--- pulsar-standalone.sh
+++ pulsar-standalone.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
+
+# Run Apache Pulsar in standalone mode using Docker
 docker run -it -p 6650:6650 -p 8080:8080 \
 --mount source=pulsardata,target=/pulsar/data \
 --mount source=pulsarconf,target=/pulsar/conf \
-apachepulsar/pulsar:2.7.0 ../bin/pulsar standalone
+apachepulsar/pulsar:2.7.0 bin/pulsar standalone
+
