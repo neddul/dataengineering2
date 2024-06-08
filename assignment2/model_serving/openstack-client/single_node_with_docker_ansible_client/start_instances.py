2fc6925727a80640960a7bd6cb14c04c6f22a320
neddul <ludvigwesterholm98@gmail.com>
2024-06-08 07:46:07 +0000

    setup start_instance

--- assignment2/model_serving/openstack-client/single_node_with_docker_ansible_client/start_instances.py
+++ assignment2/model_serving/openstack-client/single_node_with_docker_ansible_client/start_instances.py
@@ -9,11 +9,12 @@ from keystoneauth1 import loading
 from keystoneauth1 import session
 
 
-flavor = "ssc" 
-private_net = "SNIC Network"
+flavor = "ssc.medium" 
+private_net = "UPPMAX 2023/1-1 Internal IPv4 Network"
 floating_ip_pool_name = None
 floating_ip = None
-image_name = "image-ID"
+image_name = "Ubuntu 22.04 - 2023.01.07"
+key_name="cluster-key"
 
 identifier = random.randint(1000,9999)
 
@@ -58,8 +59,8 @@ else:
 secgroups = ['default']
 
 print ("Creating instances ... ")
-instance_prod = nova.servers.create(name="prod_server_with_docker_"+str(identifier), image=image, flavor=flavor, key_name='<KEY-NAME>',userdata=userdata_prod, nics=nics,security_groups=secgroups)
-instance_dev = nova.servers.create(name="dev_server_"+str(identifier), image=image, flavor=flavor, key_name='<KEY-NAME>',userdata=userdata_dev, nics=nics,security_groups=secgroups)
+instance_prod = nova.servers.create(name="lw_2_3_prod_server_with_docker_"+str(identifier), image=image, flavor=flavor, key_name=key_name, userdata=userdata_prod, nics=nics,security_groups=secgroups)
+instance_dev = nova.servers.create(name="lw_2_3_dev_server_"+str(identifier), image=image, flavor=flavor, key_name=key_name, userdata=userdata_dev, nics=nics,security_groups=secgroups)
 inst_status_prod = instance_prod.status
 inst_status_dev = instance_dev.status
 
