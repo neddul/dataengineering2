364d6333459711bd41bc3ce8c6c600fe2c0ad5bd
neddul <ludvigwesterholm98@gmail.com>
2024-06-09 11:01:57 +0000

    changed .yml file so that the composer will use correct directory

--- assignment3/create_more_nodes_with_docker/start_instance.py
+++ assignment3/create_more_nodes_with_docker/start_instance.py
@@ -8,7 +8,7 @@ import keystoneclient.v3.client as ksclient
 from keystoneauth1 import loading
 from keystoneauth1 import session
 
-flavor = "ssc.medium" 
+flavor = "ssc.small" 
 private_net = "UPPMAX 2024/1-4 Internal IPv4 Network"
 floating_ip_pool_name = None
 floating_ip = None
